0be06438d6aa26a7d776dbf8c711f7fee0c75864
max.chen <amingccc@gmail.com>
2018-12-14 14:54:39 +0800

    修改包名称

--- base_project/src/main/java/com/xiaoshu/backendframework/service/SendMailSevice.java
+++
@@ -1,8 +0,0 @@
-package com.xiaoshu.backendframework.service;
-
-import javax.mail.MessagingException;
-
-public interface SendMailSevice {
-
-    void sendMail(String u, String subject, String content) throws MessagingException;
-}
